99cb1496ab35cbdad331b39f458a176b91c05e2e
amibats <ami.keepsake@gmail.com>
2017-02-25 15:14:16 +0530

    Add files via upload

--- src/android/src/com/keepsake/cordova/plugin/UHFReader.java
+++ src/android/src/com/keepsake/cordova/plugin/UHFReader.java
@@ -8,7 +8,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import ivrjacku1.rfid.ivrjacku1.*;
+import ivrjacku1.*;
 
 public class UHFReader extends CordovaPlugin {
 
