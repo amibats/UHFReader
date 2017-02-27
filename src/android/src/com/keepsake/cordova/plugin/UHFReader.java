b7a55462deceaba8b16ea744bcc5125f64b6c5ce
amibats <ami.keepsake@gmail.com>
2017-02-27 16:48:53 +0530

    Add files via upload

--- src/android/src/com/keepsake/cordova/plugin/UHFReader.java
+++ src/android/src/com/keepsake/cordova/plugin/UHFReader.java
@@ -1,90 +1,72 @@
 package com.keepsake.cordova.plugin;
 
 import org.apache.cordova.*;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.content.Context;
 
-import android.*;
-// import android.content.Intent;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast; 
+
 import rfid.ivrjacku1.*;
-import java.util.*;
 
 public class UHFReader extends CordovaPlugin implements IvrJackAdapter {
 
 	private Context andContext;
 	private IvrJackService ivrjacku1;
-	private CallbackContext callbackContext;
-	// private static final String READ_INTENT = "keepsake.intent.action.READ";
-	// public static final int READ_CODE = 0;
+	
 
-	String [] permissions = { Manifest.permission.RECORD_AUDIO };
-
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-		super.initialize(cordova, webView);
-		if (hasPermisssion()) {
-			PluginResult r = new PluginResult(PluginResult.Status.OK);
-			callbackContext.sendPluginResult(r);
-		} else {
-			PermissionHelper.requestPermissions(this, 0, permissions);
-		}
-	}
-
-	public boolean hasPermisssion() {
-		for (String p : permissions) {
-			if (!PermissionHelper.hasPermission(this, p)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private IvrJackService getIvrJackService() {
-		if (ivrjacku1 == null) {
-			ivrjacku1 = new IvrJackService();
-		}
+	private IvrJackService getIvrJackService(){
+		if(ivrjacku1==null)
+			ivrjacku1=new IvrJackService();
 		return ivrjacku1;
 	}
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		this.andContext = this.cordova.getActivity().getApplicationContext(); 
-		getIvrJackService().open(andContext, this);
-
 		System.out.println("APPMSG - Execute");
+		System.out.println("APPMSG\t ARGS JSON - "+args.toString());
+		this.andContext = this.cordova.getActivity().getApplicationContext(); 
+		//IvrJackService ivrjacku1 = new IvrJackService();
+		getIvrJackService().open(andContext, this);
+		System.out.println("APPMSG - Open result Success");
 
 		if (action.equals("read")) {
 			System.out.println("APPMSG - Read in Execute");
-			this.readTags();
+			this.readTags(args, callbackContext);
 			return true;
-		} 
+		} else if (action.equals("write")) {
+			this.writeTags(args, callbackContext);
+			return true;
+		}
 
 		return false;
 	}
 
-	public void readTags() {
-		System.out.println("APPMSG - Read Status Change");
-    }
+	private void readTags(JSONArray args, CallbackContext callbackContext) {
 
-	@Override
-	public void onStatusChange(IvrJackStatus var1) {
-		switch (var1) {
-			case ijsDetecting: 
-				System.out.println("APPMSG - Is ijsDetecting");				
-				break;
-				
-			case ijsRecognized:
-					System.out.println("APPMSG - IsRecognized");
-					int res = getIvrJackService().readEPC(false);
-					System.out.println("APPMSG - Read: " + res);
-					break;
-				
-			case ijsUnRecognized:
-				System.out.println("APPMSG - ijsUnRecognized");
-				break;
-		}
+	}
+
+	private void writeTags(JSONArray args, CallbackContext callbackContext) {
+
 	}
 
 	@Override
@@ -93,11 +75,32 @@ public class UHFReader extends CordovaPlugin implements IvrJackAdapter {
 	}
 
 	@Override
-	public void onDisconnect(){
-		System.out.println("APPMSG - onDisConnect");
-	}
+    public void onDisconnect(){
+    	System.out.println("APPMSG - onDisConnect");
+    }
 
-	public void onInventory(String var1){
-		System.out.println("APPMSG - onInventory");
-	}
+    @Override
+    public void onStatusChange(IvrJackStatus var1) {
+    	switch (var1) {
+			case ijsDetecting: 
+				System.out.println("APPMSG - Is ijsDetecting");
+				break;
+				
+			case ijsRecognized:
+					System.out.println("APPMSG - IsRecognized");
+					System.out.println("APPMSG - Read Status Change");
+					int res=getIvrJackService().readEPC(true);
+					System.out.println("APPMSG - Read: "+res);
+				break;
+				
+			case ijsUnRecognized:
+				System.out.println("APPMSG - ijsUnRecognized");
+				break;
+		}
+    }
+
+    @Override
+    public void onInventory(String var1){
+    	System.out.println("APPMSG - onInventory");
+    }
 }
